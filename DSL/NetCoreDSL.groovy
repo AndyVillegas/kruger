3b30823d2a678d6b4e843388fd9259617ac54570
AndyVillegas <35319731+AndyVillegas@users.noreply.github.com>
2022-06-25 00:26:54 -0500

    Create NetCoreDSL.groovy

---
+++ DSL/NetCoreDSL.groovy
@@ -0,0 +1,12 @@
+job('Application in .Net Core') {
+  description('Application made in .Net Core 5')
+  scm {
+    git('https://github.com/AndyVillegas/kruger.git', 'test-jenkins') { node -> 
+      node / gitConfigName('AndyVillegas')
+      node / gitConfigEmail('andyvillegas440@gmail.com')
+    }
+  }
+  triggers {
+    scm('H/7 * * * *')
+  }
+}
